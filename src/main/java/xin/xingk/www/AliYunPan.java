77fe89f906b5639879415ec7d8d235dd962f46f8
陈靖杰 <850222153@qq.com>
2021-05-10 22:37:50 +0800

    修复bug

--- src/main/java/xin/xingk/www/AliYunPan.java
+++ src/main/java/xin/xingk/www/AliYunPan.java
@@ -2,14 +2,68 @@ package xin.xingk.www;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AliYunPan extends JFrame {
+public class AliYunPan extends JFrame implements Icon {
 
     public AliYunPan() throws HeadlessException {
         setTitle("备份小助手-星空");    //设置显示窗口标题
-        setSize(400,200);    //设置窗口显示尺寸
+        // 设置标题栏的图标为
+        this.setIconImage(new ImageIcon("src/main/resources/logo.png").getImage());
+        setSize(600,350);    //设置窗口显示尺寸
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //置窗口是否可以关闭
         setVisible(true);   //设置窗口是否可见
         setLocationRelativeTo(null);    //默认居中显示
+
+        //放东西，容器
+        Container container = getContentPane();
+        //绝对定位
+        container.setLayout(null);
+
+        //按钮
+        JButton button = new JButton("点击弹出一个对话框");//创建
+        button.setBounds(30,30,200,50);
+
+        //点击这个按钮的时候,弹出一个弹窗
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MyDialog();
+            }
+        });
+
+        container.add(button);
+
     }
+
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+
+    }
+
+    @Override
+    public int getIconWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getIconHeight() {
+        return 0;
+    }
+
+    class MyDialog extends JDialog{
+        public MyDialog(){
+            this.setTitle("测试");
+            //设置弹窗可见
+            this.setVisible(true);
+            //设置弹出大小
+            this.setBounds(100,100,500,500);
+            //容器
+            Container container = this.getContentPane();
+            container.add(new JLabel("我最牛逼"));
+        }
+    }
+
 }
--- src/main/java/xin/xingk/www/App.java
+++ src/main/java/xin/xingk/www/App.java
@@ -1,33 +1,39 @@
 package xin.xingk.www;
 
+import xin.xingk.www.common.CommonConstants;
+import xin.xingk.www.common.utils.AliYunPanUtil;
+import xin.xingk.www.common.utils.FileUtil;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * 微信备份程序
  *
  */
 public class App {
     public static void main( String[] args ) {
-        AliYunPan aliYunPan = new AliYunPan();
+        //AliYunPan aliYunPan = new AliYunPan();
 //        CronUtil.start();
 //        System.out.println("ok");
-        /*AliYunPanUtil.getAliYunPanInfo();//登录阿里云
+        AliYunPanUtil.getAliYunPanInfo();//登录阿里云
         String wxFileId=AliYunPanUtil.getFileId(CommonConstants.ROOT, "微信备份");//备份目录ID
-        List<String> folderList = FileUtil.fileFolderList(CommonConstants.ROOT,FileUtil.FOLDER);
-        for (String folderName :  folderList) {
-            System.out.println("folder:"+folderName);
-            String path = CommonConstants.PATH + "\\" + folderName;//路径
-            List<String> fileList = FileUtil.fileFolderList(CommonConstants.ROOT,FileUtil.FILE);//本地文件夹下文件
+        List<String> folderList = FileUtil.fileFolderList(CommonConstants.PATH,FileUtil.FOLDER);
+        for (String folder :  folderList) {
+            System.out.println("folder:"+folder);
+            List<String> fileList = FileUtil.fileFolderList(folder,FileUtil.FILE);//本地文件夹下文件
             for (String filePath :  fileList) {
                 Map<String, Object> map = FileUtil.getFileInfo(filePath);
                 String type = map.get("type").toString();
                 String typeFileId=AliYunPanUtil.getFileId(wxFileId,type);//微信备份-类型
-                String dateFileId=AliYunPanUtil.getFileId(typeFileId,folderName);//微信备份-类型-日期
+                String dateFileId=AliYunPanUtil.getFileId(typeFileId,folder);//微信备份-类型-日期
                 AliYunPanUtil.doUploadFile(dateFileId,map);
             }
             String folderFileId = AliYunPanUtil.getFileId(wxFileId, "文件夹");//微信备份-文件夹
-            String dateFileId = AliYunPanUtil.getFileId(folderFileId, folderName);//微信备份-文件夹-日期
-            AliYunPanUtil.scanFolders(path,dateFileId,false);
+            String dateFileId = AliYunPanUtil.getFileId(folderFileId, folder);//微信备份-文件夹-日期
+            AliYunPanUtil.scanFolders(folder,dateFileId,false);
         }
-        System.out.println("ok");*/
+        System.out.println("ok");
     }
 
     public static void Test(){
